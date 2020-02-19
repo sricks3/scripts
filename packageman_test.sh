6737342b41cee206ec5a494be7a24c5887fdee3f
Steven Ricks <sricks3@gmail.com>
2020-02-18 22:48:29 -0500

    Attempt to identify package manager and echo it.

--- packageman_test.sh
+++ packageman_test.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+# Attempt to determine what package manager is used on this system.
+if [[ -e $(which apt) ]]; then
+  pm=apt
+elif [[ -e $(which pacman) ]]; then
+  pm=pacman
+elif [[ -e $(which dnf) ]]; then
+  pm=dnf
+elif [[ -e $(which yum) ]]; then
+  pm=yum
+elif [[ -e $(which yay) ]]; then
+  pm=yay
+fi
+
+echo $pm
