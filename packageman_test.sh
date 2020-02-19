5e92a7e791fbf82be19057fe66e7ced53f0cf7da
Steven Ricks <sricks3@gmail.com>
2020-02-18 23:46:59 -0500

    announce which package managers are listed

--- packageman_test.sh
+++ packageman_test.sh
@@ -3,6 +3,10 @@
 # Define array of all package managers
 all_pms=(apt yum dnf pacman yay brew snap flatpack)
 
+# Let the user know which package managers are listed.
+echo This script supports the following package managers:
+echo ${all_pms[*]}
+
 # Declare array to hold package managers installed on this system
 declare -a these_pms
 # Fill array of package managers installed on this system
@@ -13,19 +17,6 @@ do
     these_pms+=($i)
   fi
 done
-
-# Pick which package manager to use
-###############################################################################
-# TODO: MAKE THIS ASK FOR USER INPUT IF MULTIPLE PMS ARE INSTALLED
-###############################################################################
-if [[ ${#these_pms[@]} < 1 ]]
-then
-  use_pm="unknown"
-else
-  use_pm=${these_pms[0]}
-fi
-
-echo $use_pm
 
 # Run a full system upgrade using all installed package managers
 ###############################################################################
